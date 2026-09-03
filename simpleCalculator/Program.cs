68f7d25539c81de6a6f44ab01635b05a2d5a6d73
jmpau <jmpau@JMP_WORK>
2026-09-03 16:29:25 -0600

    Add project files.

---
+++ simpleCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿//Jonathan Paul
+//RCET2265
+//Fall 2026
+//Simple Calculator
+//
+
+namespace simpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
